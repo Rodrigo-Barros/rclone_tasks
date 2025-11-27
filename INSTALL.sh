7552296bc0b7b07d1c90581f7514c101d5616a41
Rodrigo Barros <rodrigogenio12@gmail.com>
2025-11-27 18:34:32 -0300

    Installer improved.

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
+set -xe
 PROJECT_ROOT=$(realpath $(dirname $0))
 BIN_DIR=${BIN_DIR:-"$HOME/.local/bin"}
 CONFIG_DIR=${CONFIG_DIR:-"$HOME/.config/rclone_tasks"}
 SYSTEMD_USER_DIR=${SYSTEMD_USER_DIR:-"$HOME/.config/systemd/user"}
 RCLONE_CONFIG_CRYPT=${RCLONE_CONFIG_CRYPT:-"yes"}
-
-sed -i "s|@INSTALL_DIR|${BIN_DIR}|g" rclone.service
-sed -i "s|@CONFIG_DIR|${CONFIG_DIR}|g" rclone.service
 
 if [ ! -d "${BIN_DIR}" ];then
     mkdir -p "${BIN_DIR}"
@@ -21,11 +19,16 @@ case $RCLONE_CONFIG_CRYPT in
     *) SERVICE_FILE="$PROJECT_ROOT/service_templates/rclone.service_wo_env";;
 esac
 
-cp "$PROJECT_ROOT/rclone_tasks.py" "$INSTALL_DIR/rclone_tasks"
-cp "$PROJECT_ROOT/rclone_tasks.json" "$CONFIG_DIR/rclone_tasks.json"
-cp "$SERVICE_FILE" "$SYSTEMD_USER_DIR/rclone.service"
+cp "$SERVICE_FILE" "rclone.service"
 
-chmod +x "$INSTALL_DIR/rclone_tasks"
+sed -i "s|@INSTALL_DIR|${BIN_DIR}|g" rclone.service
+sed -i "s|@CONFIG_DIR|${CONFIG_DIR}|g" rclone.service
+
+cp "$PROJECT_ROOT/rclone_tasks.py" "$BIN_DIR/rclone_tasks"
+cp "$PROJECT_ROOT/rclone_tasks.json" "$CONFIG_DIR/rclone_tasks.json"
+cp "rclone.service" "$SYSTEMD_USER_DIR/rclone.service"
+
+chmod +x "$BIN_DIR/rclone_tasks"
 
 systemctl --user daemon-reload
 systemctl --user enable --now rclone.service
